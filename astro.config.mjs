0fe1806b2c37606ba7f4e39e73addb3c56954009
Ian Kollipara <ian.kollipara@gmail.com>
2025-01-01 14:12:31 -0600

    Removed Tech Builders Redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,4 @@ import robotsTxt from "astro-robots-txt";
 export default defineConfig({
   site: "https://cune-programming.github.io",
   integrations: [tailwind(), alpinejs(), sitemap(), robotsTxt()],
-  redirects: {
-    '/': '/specials/tech-builders-challenge'
-  }
 });
